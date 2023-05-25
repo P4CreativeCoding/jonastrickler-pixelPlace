f1dde62395345d2218c01a1914a928a4b6ba207c
Jona Strickler <jona.strickler@web.de>
2023-05-25 13:33:28 +0200

    implemented socket.io

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+// const express = require("express");
+// const app = express();
+// const port = 8080;
+// const socketIo = require("socket.io");
+// const http = require("http");
+// const server = http.createServer(app);
+// const io = socketIo(server);
+
 const express = require("express");
 const app = express();
-const port = 3000;
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const port = process.env.PORT || 3000;
+server.listen(port, function () {
+  console.log(`App listening on port: ${port}`);
+});
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + '/public'));
 
-app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+// app.listen(port, () => {
+//   console.log(`App listening on port: ${port}`);
+// });
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
 });
