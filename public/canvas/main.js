1b10eee3a20911a2eacd014427e6c5392210947b
Jona Strickler <jona.strickler@web.de>
2023-06-06 13:55:33 +0200

    jest unit test for rgba decoding function

--- public/canvas/main.js
+++ public/canvas/main.js
@@ -1,36 +1,40 @@
-const rgbas = [
-	[0, 0, 0, 255],
-	[137, 141, 144, 255],
-	[255, 255, 255, 255],
-	[212, 215, 217, 255],
-	[156, 105, 38, 255],
-	[255, 153, 170, 255],
-	[180, 74, 192, 255],
-	[129, 30, 159, 255],
-	[81, 233, 244, 255],
-	[54, 144, 234, 255],
-	[36, 80, 164, 255],
-	[126, 237, 86, 255],
-	[0, 163, 104, 255],
-	[255, 214, 53, 255],
-	[255, 168, 0, 255],
-	[255, 69, 0, 255],
-];
 const container = document.querySelector(".canvas-container");
 container.style.transform = "scale(6, 6)";
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const colorOptions = document.querySelectorAll(".color-option");
 
+function toRGBA(data) {
+	const rgbaValues = [
+		[0, 0, 0, 255],
+		[137, 141, 144, 255],
+		[255, 255, 255, 255],
+		[212, 215, 217, 255],
+		[156, 105, 38, 255],
+		[255, 153, 170, 255],
+		[180, 74, 192, 255],
+		[129, 30, 159, 255],
+		[81, 233, 244, 255],
+		[54, 144, 234, 255],
+		[36, 80, 164, 255],
+		[126, 237, 86, 255],
+		[0, 163, 104, 255],
+		[255, 214, 53, 255],
+		[255, 168, 0, 255],
+		[255, 69, 0, 255],
+	];
+	let rgbaArr = [];
+	data.forEach((color) => {
+		const rgba = rgbaValues[color];
+		rgbaArr = rgbaArr.concat(rgba);
+	});
+	return rgbaArr;
+}
+
 fetch("https://pixelplace.adaptable.app/api/canvas")
 	.then((res) => res.json())
 	.then((data) => {
-		let canvasImg = [];
-		data.forEach((color) => {
-			const rgba = rgbas[color];
-			canvasImg = canvasImg.concat(rgba);
-		});
-		const imgData = new ImageData(new Uint8ClampedArray(canvasImg), 128);
+		const imgData = new ImageData(new Uint8ClampedArray(toRGBA(data)), 128);
 		ctx.putImageData(imgData, 0, 0);
 	})
 	.catch((err) => console.error(err));
