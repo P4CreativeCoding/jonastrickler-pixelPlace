f13ca83f939d448dea2a9c7835fca4d648a4ed11
Nico Schulz <nico.schulz@stud.h-da.de>
2023-06-06 12:05:42 +0200

    changed to localhost
    Man könnte den issue mit dem dragging eventuell mit dem e.preventDefault(); lösen

--- public/main.js
+++ public/main.js
@@ -3,26 +3,26 @@ const password = document.getElementById("password");
 const form = document.getElementById("auth-form");
 
 form.addEventListener("submit", (e) => {
-	e.preventDefault();
+  e.preventDefault();
 
-	fetch("https://pixelplace.adaptable.app/login", {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			username: username.value,
-			password: password.value,
-		}),
-	})
-		.then((res) => res.json())
-		.then((data) => {
-			console.log(data);
-			if (data.valid === true) {
-				window.location.href = "/canvas";
-			} else if (data.valid === false) {
-				username.value = "";
-				password.value = "";
-			}
-		});
+  fetch("http://localhost:3000/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username: username.value,
+      password: password.value,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      if (data.valid === true) {
+        window.location.href = "/canvas";
+      } else if (data.valid === false) {
+        username.value = "";
+        password.value = "";
+      }
+    });
 });
